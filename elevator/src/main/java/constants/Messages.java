4f2231728057c1f088615f76c3636ec8f2cf9b50
BarysDrahunou <borisps93@mail.ru>
2020-11-22 18:33:54 +0300

    add readme.md, Oleg, PLEASE, take into account it.

--- elevator/src/main/java/constants/Messages.java
+++ elevator/src/main/java/constants/Messages.java
@@ -7,6 +7,7 @@ public final class Messages {
     public static final String IS_ALL_STATUSES_COMPLETED = "All statuses completed: %s";
     public static final String IS_DESTINATION_CORRECT = "Correct destination: %s";
     public static final String ARE_ALL_PEOPLE_WERE_TRANSPORTED = "All people were transported: %s";
+
     public static final String FILE_NOT_FOUND_MESSAGE = "File not found: %s";
     public static final String ERROR_LOADING_RESOURCES_MESSAGE = "Can't load resources from file: %s";
     public static final String INCORRECT_PROPERTY_VALUE = "Incorrect value of property: %s - %s";
--- elevator/src/main/java/task/PassengerTransportationTask.java
+++ elevator/src/main/java/task/PassengerTransportationTask.java
@@ -12,12 +12,14 @@ public class PassengerTransportationTask implements Runnable {
     public PassengerTransportationTask(Passenger passenger, Controller controller) {
         this.passenger = passenger;
         this.controller = controller;
+
         passenger.setTransportationState(TransportationState.IN_PROGRESS);
     }
 
     public void run() {
         controller.sitPassenger(passenger);
         controller.releasePassenger(passenger);
+
         passenger.setTransportationState(TransportationState.COMPLETED);
     }
 }
